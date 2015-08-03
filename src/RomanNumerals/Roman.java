56b6f55ae3f032561067653b1afa9d7cbdd102e5
Devin Kirby <devinckirby@gmail.com>
2015-08-03 09:35:52 -0400

    first passing test

--- src/RomanNumerals/Roman.java
+++ src/RomanNumerals/Roman.java
@@ -1,5 +1,9 @@
 package RomanNumerals;
 
 public class Roman {
+	
+	public static String convert(int number){
+		return "I";
+	}
 
 }
--- src/tests/RomanTests.java
+++ src/tests/RomanTests.java
@@ -1,11 +1,12 @@
 package tests;
 import static org.junit.Assert.*;
-
 import org.junit.*;
+import RomanNumerals.Roman;
+
 public class RomanTests {
 	@Test
 	public void testName() throws Exception {
-		assertEquals(1, Roman.convert(1));
+		assertEquals("I", Roman.convert(1));
 	}
 
 }
